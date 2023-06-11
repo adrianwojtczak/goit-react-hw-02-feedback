85ec925a5273defc8de3e670c5817d73c4726393
Adrian Wojtczak <117528537+adrianwojtczak@users.noreply.github.com>
2023-06-11 18:03:56 +0200

    Add functionality

---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const Section = ({ title, children }) => (
+  <div>
+    <h2>{title}</h2>
+    {children}
+  </div>
+);
+
+Section.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+};
